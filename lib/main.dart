2001ddcad50effe4d5fbad64966fea0fc7b377a0
Antonio Soler <asolermaria@gmail.com>
2023-11-09 18:02:49 +0100

    9/11

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:tfg/widgets/articulo.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const Perfil());
 }
 
 class MyApp extends StatelessWidget {
@@ -9,12 +10,92 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Material App',
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World'),
+        appBar: AppBar(
+          title: Text("Mundo móvil"),
         ),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: const <Widget>[
+              DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                ),
+                child: Text(
+                  'Drawer Header',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                  ),
+                ),
+              ),
+              ListTile(
+                leading: Icon(Icons.message),
+                title: Text('Messages'),
+              ),
+              ListTile(
+                leading: Icon(Icons.account_circle),
+                title: Text('Profile'),
+              ),
+              ListTile(
+                leading: Icon(Icons.settings),
+                title: Text('Settings'),
+              ),
+            ],
+          ),
+        ),
+        body: SafeArea(
+          child: Articulo(),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(
+                (Icons.home),
+              ),
+              label: "Principal",
+              backgroundColor: Colors.blue,
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                (Icons.discount),
+              ),
+              label: "Promociones",
+              backgroundColor: Colors.amber,
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                (Icons.shopping_cart),
+              ),
+              label: "Carrito",
+              backgroundColor: Colors.amber,
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                (Icons.person),
+              ),
+              label: "Perfil",
+              backgroundColor: Colors.amber,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Perfil extends StatelessWidget {
+  const Perfil({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+        body: SafeArea(child: Articulo()), //TOCAR A PARTIR DE AQUI
       ),
     );
   }
---
+++ lib/widgets/articulo.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class Articulo extends StatelessWidget {
+  const Articulo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(20),
+      child: Container(
+        padding: EdgeInsets.all(10),
+        decoration:
+            BoxDecoration(border: Border.all(color: Colors.black, width: 3)),
+        child: Column(
+          children: [
+            Image(
+              image: NetworkImage(
+                  "https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg"),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                "Iphone 12",
+              ),
+            ),
+            Container(
+              alignment: Alignment.centerRight,
+              child: Text("200€"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
