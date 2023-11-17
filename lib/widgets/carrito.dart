cd3f994c549cbee28ce1ef346a00dfd5d1399a77
Antonio Soler <asolermaria@gmail.com>
2023-11-17 15:13:06 +0100

    17/11 v2

--- lib/widgets/carrito.dart
+++ lib/widgets/carrito.dart
@@ -6,7 +6,49 @@ class Carrito extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return /*Container(
+      margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
+      decoration: BoxDecoration(
+          border: Border.all(color: Colors.black, width: 2),
+          borderRadius: BorderRadius.all(Radius.circular(10))),
+      child: Column(
+        children: [
+          Container(
+            child: Row(
+              children: [
+                Text("Total:"),
+                Text("100€"),
+                ElevatedButton(
+                  onPressed: () {
+                    // Acción a realizar cuando se presiona el botón
+                    print('Botón presionado');
+                  },
+                  child: Text("Comprar"),
+                ),
+              ],
+            ),
+          ),
+          GridView.count(
+            crossAxisCount: 1,
+            padding: EdgeInsets.all(20),
+            mainAxisSpacing: 10,
+            childAspectRatio: 2.5,
+            children: <Widget>[
+              articuloCarrito(),
+              articuloCarrito(),
+              articuloCarrito(),
+              articuloCarrito(),
+              articuloCarrito(),
+              articuloCarrito(),
+              articuloCarrito(),
+              articuloCarrito(),
+            ],
+          ),
+        ],
+      ),
+    );*/
+
+        Container(
       margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
       decoration: BoxDecoration(
           border: Border.all(color: Colors.black, width: 2),
